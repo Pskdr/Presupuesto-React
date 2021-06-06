f57c84c6b1eb5677a02bc50ab7782f4a212b4e40
Pskdr <juanca.naranjo03@gmail.com>
2021-06-06 13:21:14 -0500

    Mostrando mensajes de error

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,14 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
 const Pregunta = () => {
+    //definir el state
+    const[ cantidad, guardarCantidad ] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    //funcion que lee el presupuesto
+
+    const definirPresupuesto = e =>{
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e =>{
+        e.preventDefault();
+
+        //validar
+        if(cantidad < 1){
+            guardarError(true);
+            return;
+        }
+
+        // si se pasa la validación
+        guardarError(false);
+    }
+
+
     return (  
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+
+            {error ? <Error mensaje='el presupuesto es incorrecto'/> : null}
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
                 <input
                     type="submit"
